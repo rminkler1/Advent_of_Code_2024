aeba814055d269b57466e7735e864395321b45eb
robertminkler <robertminkler@gmail.com>
2024-12-12 22:15:12 -0700

    Refine Day 3 Solution

--- day3_1.py
+++ day3_1.py
@@ -1,22 +1,25 @@
-import re
+import re   # Regex magic
 
-mult_list = []
 result = 0
 
 with open("day_3_input.txt", "r") as txt:
+
+    # read each line of the file
     next_line = txt.readline()
 
     while next_line:
+        # Regex to pull out all matching results into an array
+        # "mul(##,##)" or "do()" or "don't()"
         this_line_list = re.findall(r"mul\([0-9]+,[0-9]+\)", next_line)
-        mult_list.extend(this_line_list)
+
+        # Process the current line and add the results to result
+        for entry in this_line_list:
+            numbers = entry.strip("mul()")
+            split_numbers = numbers.split(",")
+
+            result += int(split_numbers[0]) * int(split_numbers[1])
+
+        # Get the next line to continue
         next_line = txt.readline()
 
-for entry in mult_list:
-    numbers = entry.strip("mul()")
-    split_numbers = numbers.split(",")
-
-    result += int(split_numbers[0]) * int(split_numbers[1])
-
 print(result)
-
-
--- day3_2.py
+++ day3_2.py
@@ -1,33 +1,30 @@
-import re
+import re   # Regex magic
 
-mult_list = []
-enabled = True
+enabled = True  # switch to determine if we skip or process results.
 result = 0
 
 with open("day_3_input.txt", "r") as txt:
+
+    # read each line of the file
     next_line = txt.readline()
 
     while next_line:
+        # Regex to pull out all matching results into an array
+        # "mul(##,##)" or "do()" or "don't()"
         this_line_list = re.findall(r"mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)", next_line)
-        mult_list.extend(this_line_list)
+
+        # Process the current line and add the results to result
+        for entry in this_line_list:
+            if entry == "do()":
+                enabled = True
+            elif entry == "don't()":
+                enabled = False
+            elif enabled:
+                numbers = entry.strip("mul()")
+                split_numbers = numbers.split(",")
+                result += int(split_numbers[0]) * int(split_numbers[1])
+
+        # Get the next line to continue
         next_line = txt.readline()
 
-for entry in mult_list:
-    if entry == "do()":
-        enabled = True
-    elif entry == "don't()":
-        enabled = False
-    elif enabled:
-         numbers = entry.strip("mul()")
-         split_numbers = numbers.split(",")
-         result += int(split_numbers[0]) * int(split_numbers[1])
-
-
-    # numbers = entry.strip("mul()")
-    # split_numbers = numbers.split(",")
-    #
-    # result += int(split_numbers[0]) * int(split_numbers[1])
-
 print(result)
-
-
